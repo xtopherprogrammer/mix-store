2deb8fa96918ea011d93b1064e88943fd2350978
xtophercoder <87921696+xtophercoder@users.noreply.github.com>
2023-11-15 02:52:14 +0100

    Define color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+  colors: {
+    bgColor: "#212529",
+    textColor: "#e5e5e5",
+    orange: "#ee6c4d",
+    gradient1: "#354f52",
+    gradient2: "#84a98c",
+  },
+};
