62531e87204de371287d2fc714fe207cfb567e74
xtopherprogrammer@gmail.com <xtopherprogrammer@gmail.com>
2024-01-08 11:29:50 +0100

    Fix LoginContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,1 @@
-// import HomPage from "./pages/HomPage";
-import LoginPage from "./pages/LoginPageContainer";
 
-function App() {
-  return (
-    <>
-      {/* <HomPage /> */}
-      <LoginPage />
-    </>
-  );
-}
-
-export default App;
